62d72e26b7a342f1ec89a824ebb1dd2513849b71
danielfscosta <daniel_f_costa@hotmail.com>
2023-05-16 16:38:42 +0000

    Arquivos IaC v.1

--- iac1.sh
+++ iac1.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "Criação dos diretórios..."
+echo -e "Enunciado: \n"
+echo -e "Diretórios:"
+echo -e "       /publico ;\n     /adm ;\n        /ven ;\n        /sec ; \n \n"
+echo -e "Grupos:"
+echo -e "       GRP_ADM ;\n      GRP_VEN ;\n     GRP_SEC; \n \n"
+echo -e "Usuários: \n"
+echo -e "       Departamento administrativo:\n          carlos ;\n              maria ;\n               joao; \n \n"
+echo -e "       Departamento de vendas:\n               debora ;\n              sebastiana ;\n          roberto; \n \n"
+echo -e "       Departamento de secretaria:\n           josefina ;\n            amanda ;\n              rogerio \n \n"
+echo -e "Definições: \n"
+echo -e "       - Todo provisionamento deve ser feito em um arquivo do tipo Bash Script;"
+echo -e "       - O dono de todos os diretórios criados será o usuário root;"
+echo -e "       - Todos os usuários terão permissão total dentro do diretório /publico;"
+echo -e "       - Os usuarios de cada grupo terão permissão total dentro de seu respectivo diretório;"
+echo -e "       - Os usuários não poderão ter permissão de leitura, escrita e execução em diretórios de departamentos que eles não pertencem; \n \n"
+
+echo "Criando diretórios..."
 
 mkdir /publico
 mkdir /adm
@@ -41,7 +57,3 @@ useradd amanda -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
 
 echo "Execução finalizada do script."
-
-
-
-
